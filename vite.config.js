f4585ffcecf22b2cf30af7744c7c6a4d24eed1ac
tvinchent <tvinchent@gmail.com>
2023-12-17 00:15:11 +0100

    change base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://pictionai.fr/bca/',
+  base: 'https://pictionai.fr/bca-app/',
   plugins: [react()],
 })
